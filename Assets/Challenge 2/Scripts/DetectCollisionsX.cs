6969855027ae653b2f20e542ebd9e6dc0a3423ac
Carla Aluvai <carngie@gmail.com>
2022-08-24 14:43:22 +0300

    Add project files

---
+++ Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float VerticalInput;
+    public float speed = 40.0f;
+    private float sendTime = 0.0f;
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        VerticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * VerticalInput * Time.deltaTime * speed);
+        SendDog();
+        
+
+    }
+
+       // On spacebar press, send dog
+    public void SendDog() {
+        
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > sendTime) {
+            sendTime = Time.time + 2.0f;
+
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            
+            }
+       }
+}
+
